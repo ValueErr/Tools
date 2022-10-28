b1bc26364beaaa0276e583d67b571f94caebc022
Tiago <iitiagoclp@gmail.com>
2022-10-28 17:28:02 +0100

    added csv export

--- whmCheck/main.py
+++ whmCheck/main.py
@@ -1,5 +1,9 @@
+from xml import dom
 import config
 import functions
+import csv
+
+domainsALL = []
 
 # Main Loop
 for server in config.SERVERS:
@@ -13,5 +17,16 @@ for server in config.SERVERS:
             #Check SERVERIP is not apart of domain's A records + check to see if Cloudflare is in use + check to see its not already suspended
             if SERVERIP not in domainINFO['aRecords'] and domainINFO['usingcloudflare'] is False and config.SERVERS[server].domains[domain] != 1:
                 print(domain, domainINFO)
+                domainsALL.append([server, domain])
         else:
             print(domain, 'No Records Found')
+
+with open ('data.csv', 'w') as f:
+
+    fieldnames = ['server', 'domain']
+
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for domain in domainsALL:
+        writer.writerow({'server': domain[0], 'domain': domain[1]}) 
